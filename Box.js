e73153d9cb05b54a662f3ce40323ff33bf0e1e0b
AaronSunil <72694245+AaronSunil@users.noreply.github.com>
2021-03-29 11:43:29 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -8,15 +8,11 @@ class Box{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.visibility = 255;
         World.add(world, this.body);
       }
       display(){
-        World.remove(world, this.body);
         push();
-        this.visibility = this.visibility*5;
-        tint(255,this.visibility);
-        image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+        translate(this.body.position.x,this.body.position.y);
         pop();
       }
 }
--- sketch.js
+++ sketch.js
@@ -3,19 +3,37 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-
 var engine, world;
-var ground;
+var ground, stand;
+//level one
+var block1,block2,block3,block4,block5,block6,block7;
+//level two
+var block8,block9,block10,block11,block12;
+//level three
+var block13,block14,block15;
+//top level
+var block16;
 
 function preload() {
 
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-    ground = new Ground(600,height,1200,20)
+
+    ground = new Ground(600,height,1200,20);
+    stand1 = new Ground(600,350,300,20);
+    stand2 = new Ground(1000,200,300,20)
+
+    block1 = new Box(160,325,30,40);
+    block2
+    block3
+    block4
+    block5
+    block6
+    block7
 }
 
 function draw(){
@@ -23,4 +41,7 @@ function draw(){
     Engine.update(engine);
     strokeWeight(4);
     ground.display();
+    stand1.display();
+    stand2.display();
+    block1.display();
 }
